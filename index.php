c49f3a9f898036939f3810cda045157e9043e143
codingStatus.com <61621307+codingstatus@users.noreply.github.com>
2021-01-17 21:04:21 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Page Title</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+ <meta name="description" content="web page is created by html. a web page also used to create a website. a web page may contain content like images, text, video, audio">
+    <meta name="keywords" content="html page, web page, html web page, web page in html, how to make html web page, make web page in html">
+     <META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
+<link rel='shortcut icon' href="images/icon.jpg" />
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<!-- ============header start============-->
+<div class="header">
+  <div class="logo">
+  	<img src="images/logo.jpg">
+  </div>
+</div>
+<!--=============header end============= -->
+
+<!-- =============navigation bar start==============-->
+<div class="navbar">
+	<ul>
+  <li><a href="#">Home</a></li>
+  <li><a href="#">Menu-1</a></li>
+  <li><a href="#">Menu-2</a></li>
+  <li><a href="#">Menu-3</a></li>
+  <li><a href="#">Menu-4</a></li>
+  <li><a href="#">Menu-5</a></li>  
+</ul>
+</div>
+<!-- ===========navigation bar end ==============-->
+
+<!--==========main content start=======-->
+<div class="row">
+  <div class="main">
+
+  	<!--  main content start-->
+  	<div class="main-content">
+  	<h1>HTML Web Page</h1>
+  	<img src="images/banner.jpg" width="100%">
+  	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+  </div>
+
+ <!-- main content end-->
+
+
+ <!--- secondary content start-->
+ <div class="secondary-content">
+ <h2>Second Heading</h2>
+
+<p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book.Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, yedndnd v graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book.Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book.</p>
+</div>
+ <!--secondary content end-->
+
+  <!-- page list start-->
+ <h3>Web Page List</h3>
+ 	<ul>
+	<li>It is dummy text used in laying out print</li>
+	<li>The passage is attributed to an unknown</li>
+	<li>Malorum for use in a type specimen book</li>
+	<li>Finibus Bonorum et Malorum for use</li>
+	<li>This is dummy text list</li>
+	</ul>
+ <!--page list end-->
+
+ <!-- page table start-->
+ <div class="page-table">
+ <h2>Web Page Table</h2>
+ <table border="1" cellspacing="0" cellpadding="5" width="100%">
+	<thead>
+		<tr>
+			<th>Table Heading - 1</th>
+			<th>Table Heading - 2</th>
+			<th>Table Heading - 3</th>
+			<th>Table Heading - 5</th>
+			<th>Table Heading - 5</th>
+		</tr>
+		<tr>
+			<td>Row-1 Column-1 </td>
+			<td>Row-1 Column-2 </td>
+			<td>Row-1 Column-3</td>
+			<td>Row-1 Column-4 </td>
+			<td>Row-1 Column-5 </td>
+		</tr>
+			<tr>
+			<td>Row-2 Column-1 </td>
+			<td>Row-2 Column-2 </td>
+			<td>Row-2 Column-3</td>
+			<td>Row-2 Column-4 </td>
+			<td>Row-2 Column-5 </td>
+		</tr>
+				<tr>
+			<td>Row-3 Column-1 </td>
+			<td>Row-3 Column-2 </td>
+			<td>Row-3 Column-3</td>
+			<td>Row-3 Column-4 </td>
+			<td>Row-3 Column-5 </td>
+		</tr>
+
+			<tr>
+			<td>Row-4 Column-1 </td>
+			<td>Row-4 Column-2 </td>
+			<td>Row-4 Column-3</td>
+			<td>Row-4 Column-4 </td>
+			<td>Row-4 Column-5 </td>
+		</tr>
+	</thead>
+	<tbody>
+		
+	</tbody>
+</table>
+</div>
+ <!-- page table end-->
+
+ <!--- page form start-->
+ <div class="page-form">
+
+ <h2>Contact Form</h2>
+
+ <form>
+ <div class="row">
+  <div class="form-col">
+ 	<label>Full Name</label>
+ 	<input type="text" name="fullName" placeholder="Enter Full Name">
+  </div>
+
+<div class="form-col">
+  <label>Mobile Number</label>
+  <input type="email" name="email" placeholder="Enter Email">
+</div>
+</div>
+
+<div class="row">
+  <div class="form-col">
+  <label>Email</label>
+  <input type="email" name="email" placeholder="Enter Email">
+</div>
+
+<div class="form-col">
+ <label>Subject</label>
+ 	<input type="text" name="subject" placeholder="Enter Subject">
+</div>
+</div>
+
+<label>Message</label>
+ <textarea name="message" placeholder="Write Something.." rows="10">
+ </textarea>
+
+ <button type="submit" >Send</button>
+
+ 	</form>
+ </div>
+ <!---page form end-->
+
+  </div>
+
+  <!--===============main content end=============-->
+
+  <!--===========sidebar start ============-->
+   <div class="sidebar">
+
+<!-- about section start -->
+   	<div class="about">
+    <h3>About Us</h3>
+    <p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts</p>
+</div>
+<!-- about section end-->
+
+<!-- Latest post start-->
+    <div class="latest-post">
+    <h3>Latest Post</h3>
+    <ul>
+    	<li>It is dummy text used in laying out print</li>
+	<li>The passage is attributed to an unknown</li>
+	<li>Malorum for use in a type specimen book</li>
+	<li>Finibus Bonorum et Malorum for use</li>
+		<li>It is dummy text used in laying out print</li>
+	<li>The passage is attributed to an unknown</li>
+	<li>Malorum for use in a type specimen book</li>
+	<li>Finibus Bonorum et Malorum for use</li>
+</ul>
+</div>
+<!-- latest post end-->
+
+<!-- popular post start-->
+<div class="popular-post">
+<h3>Popular Post</h3>
+    <ul>
+    	<li>It is dummy text used in laying out print</li>
+	<li>The passage is attributed to an unknown</li>
+	<li>Malorum for use in a type specimen book</li>
+	<li>Finibus Bonorum et Malorum for use</li>
+		<li>It is dummy text used in laying out print</li>
+	<li>The passage is attributed to an unknown</li>
+	<li>Malorum for use in a type specimen book</li>
+	<li>Finibus Bonorum et Malorum for use</li>
+</ul>
+    </div>
+ <!--popular post end-->
+
+  </div>
+
+<!--===============sidebar end================-->
+
+<!--===============footer section============-->
+
+<div class="footer row">
+	<div class="footer-col">
+		<h3>first footer section</h3>
+		<ul>
+	<li>First footer Text</li>
+	<li>Second footer Text</li>
+	<li>Third footer Text</li>
+	<li>Fourth footer Text</li>
+	<li>Fifth footer Text</li>
+	</ul>
+	</div>
+		<div class="footer-col">
+			<h3>Second footer section</h3>
+					<ul>
+	<li>First footer Text</li>
+	<li>Second footer Text</li>
+	<li>Third footer Text</li>
+	<li>Fourth footer Text</li>
+	<li>Fifth footer Text</li>
+	</ul>
+	</div>
+		<div class="footer-col">
+			<h3>Third footer section</h3>
+					<ul>
+	<li>First footer Text</li>
+	<li>Second footer Text</li>
+	<li>Third footer Text</li>
+	<li>Fourth footer Text</li>
+	<li>Fifth footer Text</li>
+	</ul>
+	</div>
+
+ 
+</div>
+<!--===================foooter section end==============-->
+
+</body>
+</html>
